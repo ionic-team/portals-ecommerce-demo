3cd50da04af6552eb4367919399a20b054025b42
Carl Poole <carl@ionic.io>
2021-05-27 13:08:09 -0500

    Added example navigation

--- android/PortalsEcommerce/app/src/main/java/io/ionic/demo/ecommerce/ui/store/StoreFragment.java
+++ android/PortalsEcommerce/app/src/main/java/io/ionic/demo/ecommerce/ui/store/StoreFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import io.ionic.demo.ecommerce.R;
 import io.ionic.demo.ecommerce.data.DataReader;
@@ -19,10 +21,8 @@ public class StoreFragment extends Fragment {
 
     private StoreViewModel storeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        storeViewModel =
-                new ViewModelProvider(this).get(StoreViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        storeViewModel = new ViewModelProvider(this).get(StoreViewModel.class);
         View root = inflater.inflate(R.layout.fragment_store, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         storeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -31,6 +31,16 @@ public class StoreFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        // Example of navigating to a product
+        final Button testNavButton = root.findViewById(R.id.test_button);
+        testNavButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(root).navigate(R.id.navigation_product);
+            }
+        });
+
         return root;
     }
 }
