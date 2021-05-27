3c046d92c2e99cae68f85e8d468c038ca4cbbb96
Carl Poole <carl@ionic.io>
2021-05-27 09:51:39 -0500

    Added nav bar icons per design and removed menu item text

--- android/PortalsEcommerce/app/src/main/java/io/ionic/demo/ecommerce/MainActivity.java
+++ android/PortalsEcommerce/app/src/main/java/io/ionic/demo/ecommerce/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_store, R.id.navigation_cart, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
