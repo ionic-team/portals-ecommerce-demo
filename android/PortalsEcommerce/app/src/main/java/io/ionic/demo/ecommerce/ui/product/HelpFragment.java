d846d35c036765a02a622694c9a648658bc746b5
Carl Poole <carl@ionic.io>
2021-05-28 15:18:28 -0500

    Help page loading a stub web app

--- android/PortalsEcommerce/app/src/main/java/io/ionic/demo/ecommerce/ui/product/HelpFragment.java
+++ android/PortalsEcommerce/app/src/main/java/io/ionic/demo/ecommerce/ui/product/HelpFragment.java
@@ -4,18 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.getcapacitor.BridgeFragment;
+import com.getcapacitor.WebViewListener;
 
 import io.ionic.demo.ecommerce.R;
+import io.ionic.demo.ecommerce.portals.FadeBridgeFragment;
 
 public class HelpFragment extends Fragment {
 
+    BridgeFragment embeddedFragment;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_help, container, false);
-        return root;
+        return inflater.inflate(R.layout.fragment_help, container, false);
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final FragmentManager fragmentManager = getParentFragmentManager();
+        embeddedFragment = FadeBridgeFragment.newInstance("help_app", android.R.color.white, 500);
+
+        // Inflate the fragment
+        fragmentManager.beginTransaction().replace(R.id.help_web_app, embeddedFragment).commit();
+    }
 }
