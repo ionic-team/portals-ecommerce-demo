e2448e112b2be56cc94d67a7ff49a61fd0b410a1
Ian Keith <iankeith@gmail.com>
2021-06-03 18:02:33 -0400

    Updating interface.

--- web/src/ShopAPIPlugin.ts
+++ web/src/ShopAPIPlugin.ts
@@ -1,13 +1,29 @@
 import { registerPlugin } from "@capacitor/core";
 import { Cart, User } from "./models";
 
+export interface CartResult {
+  cart: Cart;
+}
+
+export interface UserResult {
+  user: User;
+}
+
+export interface CheckoutResult {
+  result: "success" | "cancel" | "failure";
+}
+
+export interface UserPicture {
+  picture: string;
+}
+
 export interface ShopAPIPlugin {
-  getCart(): Promise<Cart>;
-  getUserDetails(): Promise<User>;
-  updateUserDetails(user: User): Promise<void>;
-  checkoutResult(status: "success" | "cancel" | "failure"): Promise<void>;
-  getUserPicture(): Promise<string>;
-  setUserPicture(picture: string): Promise<void>;
+  getCart(): Promise<CartResult>;
+  getUserDetails(): Promise<UserResult>;
+  updateUserDetails(result: UserResult): Promise<void>;
+  checkoutResult(result: CheckoutResult): Promise<void>;
+  getUserPicture(): Promise<UserPicture>;
+  setUserPicture(picture: UserPicture): Promise<void>;
 }
 
 const ShopAPI = registerPlugin<ShopAPIPlugin>("ShopAPI");
