621990157de7b01301b07317d9217138237a32f4
Ian Keith <iankeith@gmail.com>
2021-06-09 19:07:01 -0400

    Adding stretchable header to product detail.

--- ios/Portals Ecommerce/Portals Ecommerce/View Controllers/ProductDetailViewController.swift
+++ ios/Portals Ecommerce/Portals Ecommerce/View Controllers/ProductDetailViewController.swift
@@ -18,7 +18,7 @@ class ProductDetailViewController: UIViewController, ApplicationCoordinationPart
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // style the button
         addButton.setBackgroundImage(view.tintColor.buttonImageWith(cornerRadius: 8), for: .normal)
         addButton.setTitleColor(.white, for: .normal)
     }
