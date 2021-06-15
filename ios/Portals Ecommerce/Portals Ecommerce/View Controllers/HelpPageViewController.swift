71eca6186dfdf6ae46ff6d8a397abe117bc83fee
Ian Keith <iankeith@gmail.com>
2021-06-15 12:40:55 -0400

    Inject coordinator on presented screens, trigger route change for portals (#34)

--- ios/Portals Ecommerce/Portals Ecommerce/View Controllers/HelpPageViewController.swift
+++ ios/Portals Ecommerce/Portals Ecommerce/View Controllers/HelpPageViewController.swift
@@ -9,6 +9,11 @@ class HelpPageViewController: HostedContentViewController {
         navigationItem.title = NSLocalizedString("Help", comment: "Help page title")
     }
     
+    override func webViewCompletedInitialLoad() {
+        super.webViewCompletedInitialLoad()
+        webView?.evaluateJavaScript("window.location.href = \"/help\"", completionHandler: nil)
+    }
+    
     override func instanceDescriptor() -> InstanceDescriptor {
         let path = Bundle.main.url(forResource: "portals/shopwebapp", withExtension: nil)!
         let descriptor = InstanceDescriptor(at: path, configuration: nil, cordovaConfiguration: nil)
