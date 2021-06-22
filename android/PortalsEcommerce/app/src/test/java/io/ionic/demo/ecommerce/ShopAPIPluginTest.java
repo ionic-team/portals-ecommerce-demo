211cf4f02b05d0ae6fe1e9923fbfd6695fc7e913
Carl Poole <carl@ionic.io>
2021-06-22 09:02:12 -0500

    Fixed image storing tests

--- android/PortalsEcommerce/app/src/test/java/io/ionic/demo/ecommerce/ShopAPIPluginTest.java
+++ android/PortalsEcommerce/app/src/test/java/io/ionic/demo/ecommerce/ShopAPIPluginTest.java
@@ -2,7 +2,11 @@ package io.ionic.demo.ecommerce;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 
+import com.getcapacitor.Bridge;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.PluginCall;
 
@@ -14,6 +18,7 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -22,18 +27,23 @@ import io.ionic.demo.ecommerce.plugins.ShopAPIPlugin;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(EcommerceApp.class)
+@PrepareForTest({EcommerceApp.class, BitmapFactory.class, Base64.class})
 public class ShopAPIPluginTest {
     private static final String APP_DATA = "data.json";
 
     Context context = mock(Context.class);
     AssetManager assetManager = mock(AssetManager.class);
     PluginCall call = mock(PluginCall.class);
+    Bridge bridge = mock(Bridge.class);
+    Bitmap bitmap = mock(Bitmap.class);
+    FileOutputStream fileOutputStream = mock(FileOutputStream.class);
 
     private InputStream getTestInputStream() {
         return this.getClass().getClassLoader().getResourceAsStream(ShopAPIPluginTest.APP_DATA);
@@ -43,9 +53,15 @@ public class ShopAPIPluginTest {
     public void init() {
         try{
             PowerMockito.mockStatic(EcommerceApp.class);
+            PowerMockito.mockStatic(BitmapFactory.class);
+            PowerMockito.mockStatic(Base64.class);
             when(context.getAssets()).thenReturn(assetManager);
-            when(assetManager.open("webapp/data.json")).thenReturn(getTestInputStream());
+            when(assetManager.open("data.json")).thenReturn(getTestInputStream());
             when(EcommerceApp.getContext()).thenReturn(context);
+            when(BitmapFactory.decodeResource(context.getResources(), R.drawable.jt_avatar)).thenReturn(bitmap);
+            when(Base64.encodeToString(any(byte[].class), eq(Base64.DEFAULT))).thenReturn("storedimagebase64");
+            when(context.openFileOutput("user-image.jpg", Context.MODE_PRIVATE)).thenReturn(fileOutputStream);
+            when(bridge.getContext()).thenReturn(context);
         } catch (IOException e) {
             fail();
         }
@@ -54,20 +70,22 @@ public class ShopAPIPluginTest {
     @Test
     public void getUserPictureShouldReturnPicture() {
         ShopAPIPlugin plugin = new ShopAPIPlugin();
+        plugin.setBridge(bridge);
         plugin.getUserPicture(call);
 
         ArgumentCaptor<JSObject> argument = ArgumentCaptor.forClass(JSObject.class);
         verify(call).resolve(argument.capture());
-        assertEquals("jt-avatar.png", argument.getValue().getString("picture"));
+        assertEquals("storedimagebase64", argument.getValue().getString("picture"));
     }
 
     @Test
     public void setUserPictureShouldStorePicture() {
-        when(call.getString("picture")).thenReturn("testimage");
+        when(call.getString("picture")).thenReturn("user-image.jpg");
 
         ShopAPIPlugin plugin = new ShopAPIPlugin();
+        plugin.setBridge(bridge);
         plugin.setUserPicture(call);
 
-        assertEquals("testimage", DataService.getInstance(context).getUser().image);
+        assertEquals("user-image.jpg", DataService.getInstance(context).getUser().image);
     }
 }
