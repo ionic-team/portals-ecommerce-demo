67660b33b609c76bd8d9460215ebae3dd8dbc5cc
Carl Poole <carl@ionic.io>
2021-06-23 10:38:10 -0500

    Swapped action bar for UI toolbar and it fixed the animation stuff

--- android/PortalsEcommerce/app/src/main/java/io/ionic/demo/ecommerce/MainActivity.java
+++ android/PortalsEcommerce/app/src/main/java/io/ionic/demo/ecommerce/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -21,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         appBarConfiguration = new AppBarConfiguration.Builder(
