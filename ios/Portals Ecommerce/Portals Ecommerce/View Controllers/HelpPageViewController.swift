698cca8c7baf0aa0c7a8cd7a0f14a3560eb628a2
giralte-ionic <97970732+giralte-ionic@users.noreply.github.com>
2022-03-09 15:59:38 -0600

    fixes for post-split (#59)
    Co-authored-by: Dan Giralté (Ionic) <giralte@ionic.com>

--- ios/Portals Ecommerce/Portals Ecommerce/View Controllers/HelpPageViewController.swift
+++ ios/Portals Ecommerce/Portals Ecommerce/View Controllers/HelpPageViewController.swift
@@ -7,19 +7,18 @@ class HelpPageViewController: AppParticipantViewController {
     
     override var requiresPreloading: Bool { return true }
 
-    override func viewDidLoad() {
-        
+    override func loadView() {
         // Do any additional setup after loading the view.
         let portal = try! PortalManager.getPortal("help")
+        
         var initialContext: [String: String] = [:]
         initialContext["startingRoute"] = "/help"
+        
         portal.initialContext = initialContext
         
-        let portalWebView = PortalWebView(frame: view.frame, portal: portal)
+        let portalWebView = PortalWebView(portal: portal)
           
         self.view = portalWebView
         self.bridge = portalWebView.bridge
-        super.viewDidLoad()
     }
-    
 }
--- ios/Portals Ecommerce/Portals Ecommerce/View Controllers/UserProfileViewController.swift
+++ ios/Portals Ecommerce/Portals Ecommerce/View Controllers/UserProfileViewController.swift
@@ -5,19 +5,18 @@ class UserProfileViewController: AppParticipantViewController {
     
     override var requiresPreloading: Bool { return true }
     
-    override func viewDidLoad() {
-        
+    override func loadView() {
         // Do any additional setup after loading the view.
         let portal = try! PortalManager.getPortal("user")
+        
         var initialContext: [String: String] = [:]
+        
         initialContext["startingRoute"] = "/user"
         portal.initialContext = initialContext
         
-        let portalWebView = PortalWebView(frame: view.frame, portal: portal)
+        let portalWebView = PortalWebView(portal: portal)
           
         self.view = portalWebView
         self.bridge = portalWebView.bridge
-        
-        super.viewDidLoad()
     }
 }
