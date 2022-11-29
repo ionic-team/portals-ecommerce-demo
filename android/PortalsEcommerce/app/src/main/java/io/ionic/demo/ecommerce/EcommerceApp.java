623e7cfe17421f10b61d0cd4161faeca269b46eb
Carl Poole <carl@ionic.io>
2022-11-29 13:04:21 -0600

    Remove ignored files

--- android/PortalsEcommerce/app/src/main/java/io/ionic/demo/ecommerce/EcommerceApp.java
+++ android/PortalsEcommerce/app/src/main/java/io/ionic/demo/ecommerce/EcommerceApp.java
@@ -69,7 +69,7 @@ public class EcommerceApp extends Application {
         shoppingCart = new ShoppingCart();
 
         // Register Portals
-        // PortalManager.register("YOUR_KEY_HERE");
+        PortalManager.register("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIzMDU2ZjBlNC1kYTFkLTQ1YWMtYWJjZi1hNDg3MzMyZTQwNGYifQ.0H_gnwXCL1Z-GtFCwQ3J9YrybMxQO56CYo3PFGzoueB56DMvKT4jiQhLzhDYKEE5GwlqX-r0H_qklYKg_jtMyK9QZ_-kTNWi6LyjrJTcgFVwxjz27PZaqZPoKWyJLotSIbBhN8BF5flunCGW8kWL4_nY6FUmswatPDgcvyPmOydr9InbEHHDUVvi9mGwy_G78BjDrl9bThezpGRseBTOI7KH5FUdXwH9DCZJ2RC4_ukTNKMqaKFh-OcD8KDBUIdSP8GE0quO7zL4qSINvxMMzpupTdQKf3Td5B1mvLlrS4kF_8VPoQtvB8JqMrmH2fa8f31fCiz1EV4Wkngb_5yC7w");
 
         // Checkout Portal
         PortalManager.newPortal("checkout")
